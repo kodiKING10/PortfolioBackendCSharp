6dcb1e35523fcfd2e0ed39c6ce2b433a35a9b10b
Henrique Cavalcante <hencavalcante99@gmail.com>
2020-07-29 15:12:02 -0300

    Skill routes

--- PortfolioBackendCSharp/Controllers/SkillController.cs
+++ PortfolioBackendCSharp/Controllers/SkillController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PortfolioBackendCSharp.DAL;
+using PortfolioBackendCSharp.Models;
 
 namespace PortfolioBackendCSharp.Controllers
 {
@@ -11,5 +14,103 @@ namespace PortfolioBackendCSharp.Controllers
     [ApiController]
     public class SkillController : ControllerBase
     {
+        #region [Get Routes]
+
+        [HttpGet]
+        public List<Skill> Get()
+        {
+            Connection conn = new Connection();
+            List<Skill> listSkill = new List<Skill>();
+            string strQuery = "SELECT * FROM Skill";
+
+            SqlDataReader data = conn.ExecuteWithReturn(strQuery);
+
+            while (data.Read())
+            {
+                Skill skill = new Skill();
+                skill.ID = data.GetInt32(0);
+                skill.SkillName = conn.CheckString(data, 1);
+                skill.Percentage = data.GetInt32(2);
+
+                listSkill.Add(skill);
+            }
+            return listSkill;
+        }
+
+        [HttpGet("{id:int}")]
+        public List<Skill> Get(int id)
+        {
+            Connection conn = new Connection();
+            List<Skill> listSkill = new List<Skill>();
+            string strQuery = "SELECT * FROM Skill WHERE ID = " + id;
+
+            SqlDataReader data = conn.ExecuteWithReturn(strQuery);
+
+            while (data.Read())
+            {
+                Skill skill = new Skill();
+                skill.ID = data.GetInt32(0);
+                skill.SkillName = conn.CheckString(data, 1);
+                skill.Percentage = data.GetInt32(2);
+
+                listSkill.Add(skill);
+            }
+            return listSkill;
+        }
+        #endregion
+
+        #region [Post Route]
+
+        [HttpPost]
+        public string Post([FromBody] Skill skill)
+        {
+            Connection conn = new Connection();
+
+            string strQuery = string.Format("INSERT INTO Skill( SkillName, Percentage ) VALUES('{0}','{1}')",
+                skill.SkillName,
+                skill.Percentage
+            );
+            conn.ExecuteWithoutReturn(strQuery);
+
+            return "Skill inserted successfully!";
+        }
+
+        #endregion
+
+        #region [Delete Route]
+
+        [HttpDelete("{id:int}")]
+        public string Delete(int id)
+        {
+            Connection conn = new Connection();
+            List<Skill> listSkill = new List<Skill>();
+            string strQuery = "DELETE FROM Skill WHERE ID = " + id;
+
+            conn.ExecuteWithoutReturn(strQuery);
+
+            return "Skill deleted successfully!";
+        }
+
+        #endregion
+
+        #region [Update Route]
+
+        [HttpPut("{id:int}")]
+        public string Put([FromBody] Skill skill, int id)
+        {
+            Connection conn = new Connection();
+
+            string strQuery = string.Format("UPDATE Skill SET SkillName = '{0}', Percentage = '{1}' WHERE ID = '{2}'",
+                skill.SkillName,
+                skill.Percentage,
+                id
+            );
+
+            conn.ExecuteWithoutReturn(strQuery);
+
+            return "Skill with ID " + id + " updated successfully!";
+        }
+
+        #endregion
     }
 }
--- PortfolioBackendCSharp/Models/Skill.cs
+++ PortfolioBackendCSharp/Models/Skill.cs
@@ -7,8 +7,10 @@ namespace PortfolioBackendCSharp.Models
 {
     public class Skill
     {
+        public int ID { get; set; }
+
         public string SkillName { get; set; }
 
-        public string Percentage { get; set; }
+        public int Percentage { get; set; }
     }
 }
